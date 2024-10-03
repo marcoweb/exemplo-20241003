c040a1e67fdefb5876c4d67c0f9e3d1f963bfd9c
Marco Antônio Gusmão Carvalho <marcocarvalho.web@gmail.com>
2024-10-03 22:46:23 +0000

    Aula 03/10/2024

--- src/main/java/application/controller/IndexController.java
+++ src/main/java/application/controller/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping("/")
     public String home() {
+        String msg = "Bem Vindo";
         return "home";
     }
 }
